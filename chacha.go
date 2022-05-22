9e85fdff68f4408b4ca8ed04d41cc411c3f386a0
blck-snwmn <whitesnowmancreator@gmail.com>
2022-05-22 17:29:55 +0900

    Add quarterRound() method in chacha state

--- chacha.go
+++ chacha.go
@@ -22,3 +22,31 @@ func quarterRound(a, b, c, d uint32) (uint32, uint32, uint32, uint32) {
 	b = rotationN(b, 7)
 	return a, b, c, d
 }
+
+type state [][]uint32
+
+func (s state) quarterRound(x, y, z, w uint) {
+	getIndex := func(v uint) (uint, uint) {
+		return v / 4, v % 4
+	}
+	getValue := func(v uint) uint32 {
+		i, j := getIndex(v)
+		return s[i][j]
+	}
+	setValue := func(v uint, value uint32) {
+		i, j := getIndex(v)
+		s[i][j] = value
+	}
+	a := getValue(x)
+	b := getValue(y)
+	c := getValue(z)
+	d := getValue(w)
+
+	a, b, c, d = quarterRound(a, b, c, d)
+
+	setValue(x, a)
+	setValue(y, b)
+	setValue(z, c)
+	setValue(w, d)
+}
+
--- chacha_test.go
+++ chacha_test.go
@@ -1,6 +1,7 @@
 package gochacha
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -76,3 +77,44 @@ func Test_quarterRound(t *testing.T) {
 		})
 	}
 }
+
+func Test_state_quarterRound(t *testing.T) {
+	type args struct {
+		x uint
+		y uint
+		z uint
+		w uint
+	}
+	tests := []struct {
+		name string
+		s    state
+		args args
+		want state
+	}{
+		{
+			name: "rfc7539 test vector",
+			s: [][]uint32{
+				{0x879531e0, 0xc5ecf37d, 0x516461b1, 0xc9a62f8a},
+				{0x44c20ef3, 0x3390af7f, 0xd9fc690b, 0x2a5f714c},
+				{0x53372767, 0xb00a5631, 0x974c541a, 0x359e9963},
+				{0x5c971061, 0x3d631689, 0x2098d9d6, 0x91dbd320},
+			},
+			args: args{2, 7, 8, 13},
+			want: [][]uint32{
+				{0x879531e0, 0xc5ecf37d, 0xbdb886dc, 0xc9a62f8a},
+				{0x44c20ef3, 0x3390af7f, 0xd9fc690b, 0xcfacafd2},
+				{0xe46bea80, 0xb00a5631, 0x974c541a, 0x359e9963},
+				{0x5c971061, 0xccc07c79, 0x2098d9d6, 0x91dbd320},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.s.quarterRound(tt.args.x, tt.args.y, tt.args.z, tt.args.w)
+			if !reflect.DeepEqual(tt.s, tt.want) {
+				t.Errorf("quarterRound() got = %x, want %x", tt.s, tt.want)
+			}
+		})
+	}
+}
+
