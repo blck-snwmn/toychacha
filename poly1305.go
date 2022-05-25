a78e85dd58f43b8c9ee0ba6d6dd189a16a7ff3c9
blck-snwmn <whitesnowmancreator@gmail.com>
2022-05-26 07:12:02 +0900

    Add convertLittleEndian()

--- poly1305.go
+++ poly1305.go
@@ -30,10 +30,14 @@ func init() {
 	p = i.Sub(shifted, big.NewInt(5))
 }
 
-func leBytesToNum(b []byte) []byte {
+func convertLittleEndian(b []byte) {
 	for i, j := 0, len(b)-1; i < j; i, j = i+1, j-1 {
 		b[i], b[j] = b[j], b[i]
 	}
+}
+
+func leBytesToNum(b []byte) []byte {
+	convertLittleEndian(b)
 	return b
 }
 
