c1a705c20eeac355ee84ed912de8d42c15c6b67a
blck-snwmn <whitesnowmancreator@gmail.com>
2022-05-28 16:28:59 +0900

    Add constructMacData()

--- aead.go
+++ aead.go
@@ -20,21 +20,36 @@ func numTo8LeBytes(l int) []byte {
 	return b
 }
 
+func size(l int) int {
+	return l + 16 - l%16
+}
+
+func constructMacData(aad, ciphertext []byte) []byte {
+	aadsize := size(len(aad))
+	ciphertextsize := size(len(ciphertext))
+
+	macData := make([]byte, aadsize+ciphertextsize+8+8)
+	header := macData
+
+	copy(header[:aadsize], aad)
+	header = header[aadsize:]
+
+	copy(header[:ciphertextsize], ciphertext)
+	header = header[ciphertextsize:]
+
+	copy(header[:8], numTo8LeBytes(len(aad)))
+	header = header[8:]
+	copy(header[:8], numTo8LeBytes(len(ciphertext)))
+	return macData
+}
+
 func aeadEncrpt(aad, key, iv, constant, plaintext []byte) ([]byte, []byte) {
 	nonce := append(constant, iv...)
 	otk := genMacKey(key, nonce)
 
 	ciphertext := encrypt(key, nonce, plaintext, 1)
 
-	// TODO 最初からサイズを決め打ちにして、あとからpaddingしなくてもよくする
-	macData := append(aad, pad16(aad)...)
-
-	macData = append(macData, ciphertext...)
-	macData = append(macData, pad16(ciphertext)...)
-
-	macData = append(macData, numTo8LeBytes(len(aad))...)
-	macData = append(macData, numTo8LeBytes(len(ciphertext))...)
-
+	macData := constructMacData(aad, ciphertext)
 	tag := mac(macData, otk)
 	return ciphertext, tag
 }
@@ -43,13 +58,7 @@ func aeadDecrypt(aad, key, iv, constant, ciphertext, tag []byte) ([]byte, error)
 	nonce := append(constant, iv...)
 	otk := genMacKey(key, nonce)
 
-	macData := append(aad, pad16(aad)...)
-
-	macData = append(macData, ciphertext...)
-	macData = append(macData, pad16(ciphertext)...)
-
-	macData = append(macData, numTo8LeBytes(len(aad))...)
-	macData = append(macData, numTo8LeBytes(len(ciphertext))...)
+	macData := constructMacData(aad, ciphertext)
 
 	calcTag := mac(macData, otk)
 	if !reflect.DeepEqual(calcTag, tag) {
