df22f9ca5323ce00963abba4ce64a7df0be694af
blck-snwmn <whitesnowmancreator@gmail.com>
2022-05-28 15:31:47 +0900

    Impl aead

---
+++ aead.go
@@ -0,0 +1,41 @@
+package gochacha
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+)
+
+func pad16(x []byte) []byte {
+	if len(x)%16 == 0 {
+		return nil
+	}
+	return bytes.Repeat([]byte{0}, 16-(len(x)%16))
+}
+
+func numTo8LeBytes(l int) []byte {
+	b := make([]byte, 8)
+	binary.LittleEndian.PutUint64(b, uint64(l))
+	return b
+}
+
+func aeadEncrpt(aad, key, iv, constant, plaintext []byte) ([]byte, []byte) {
+	nonce := append(constant, iv...)
+	otk := genMacKey(key, nonce)
+	fmt.Printf("otk=%x\n", otk)
+
+	ciphertext := encrypt(key, nonce, plaintext, 1)
+	fmt.Printf("ciphertext=%x\n", ciphertext)
+
+	// TODO 最初からサイズを決め打ちにして、あとからpaddingしなくてもよくする
+	macData := append(aad, pad16(aad)...)
+
+	macData = append(macData, ciphertext...)
+	macData = append(macData, pad16(ciphertext)...)
+
+	macData = append(macData, numTo8LeBytes(len(aad))...)
+	macData = append(macData, numTo8LeBytes(len(ciphertext))...)
+
+	tag := mac(macData, otk)
+	return ciphertext, tag
+}
